5c205c3266d278c0f4730a464c5b8b36719953d0
kudr.max <mdkudryashov@edu.hse.ru>
2024-02-17 16:49:29 +0300

    Доделал первый номрер

--- second/3.1.8.py
+++ second/3.1.8.py
@@ -9,6 +9,7 @@ N = 8
 
 os.remove(output_file_path)
 with open(output_file_path, "a") as f:
+
     # заполним матрицу A
     A = np.empty((n, n))
     for i in range(n):
@@ -27,14 +28,20 @@ with open(output_file_path, "a") as f:
     A_cond = np.linalg.cond(A, np.inf)
     print(f"{A_cond = }", file=f)
 
+    # найдем вектор множества векторов b
+    delta = 0.05
+    b_error_set = []
+    for i in range(n):
+        b_error = b.copy()
+        b_error[i] += delta
+        b_error_set.append(b_error)
+
     # найдем вектор d
-    delta = 0.01
     d = np.empty(n)
     for i in range(n):
-        bi = b.copy()
-        bi[i] += delta
-        xi = np.linalg.solve(A, bi)
-        d[i] = np.linalg.norm(x - xi, ord=np.inf) / np.linalg.norm(x, ord=np.inf)
+        b_error = b_error_set[i]
+        x_error = np.linalg.solve(A, b_error)
+        d[i] = np.linalg.norm(x - x_error, ord=np.inf) / np.linalg.norm(x, ord=np.inf)
     print(f"{d = }", file=f)
 
     # найдем что оказывает наибольшее влияние на погрешность
@@ -43,9 +50,18 @@ with open(output_file_path, "a") as f:
     print(f'{d_max = } with {d_max_index = }', file=f)
 
     # построим гистограмму
-    plt.bar(list(range(0, len(d))), d)
+    plt.bar(range(n), d)
     plt.savefig('3.1.8.png', dpi=300)
     plt.show()
+
+    print(f"delta_rel_x_error:", file=f)
+    for i in range(n):
+        b_error = b_error_set[i]
+        # delta_rel_b_old = np.linalg.norm(b - b_error, ord=np.inf) / np.linalg.norm(b, ord=np.inf)
+        delta_rel_b = np.abs(delta) / np.linalg.norm(b, ord=np.inf)
+        # @todo вывести теоретически
+        delta_rel_x_error = A_cond * delta_rel_b
+        print(f"практически: {d[i]},\tтеоритически: {delta_rel_x_error}", file=f)
 
 with open(output_file_path, "r") as f:
     print(f.read())
