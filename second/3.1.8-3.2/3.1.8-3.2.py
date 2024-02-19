06c55d8d9f19ab4118ea3daa4c4a7d38e5fae088
kudr.max <mdkudryashov@edu.hse.ru>
2024-02-19 21:42:50 +0300

    Refactored

--- second/3.1.8-3.2/3.1.8-3.2.py
+++ second/3.1.8-3.2/3.1.8-3.2.py
@@ -3,23 +3,22 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
-output_file_path = "3.2.txt"
-
 n = 6
 N = 8
-delta = 0.05
+delta = 0.0005
 
-os.remove(output_file_path)
-with open(output_file_path, "a") as f:
+with open('3.1.8-3.2.txt', "w") as f:
     # заполним матрицу A
     A = np.empty((n, n))
     for i in range(n):
         for j in range(n):
             c = 0.1 * N * (i + 1) * (j + 1)
             A[i][j] = 1 / np.sqrt(c ** 2 + 0.58 * c)
+    pd.DataFrame(A).to_csv('A.csv', float_format='%.5f')
 
     # заполним вектор b
     b = np.full(n, N, dtype=float)
+    pd.DataFrame(b).to_csv('b.csv')
 
     # найдем лист из измененных векторов b
     b_error_list = []
@@ -40,7 +39,8 @@ with open(output_file_path, "a") as f:
 
     # найдем решение через встроенную функцию
     x = np.linalg.solve(A, b)
-    print(f"{x = }", file=f)
+    # print(f"{x = }", file=f)
+    pd.DataFrame(x).to_csv('x.csv', float_format='%.6f')
 
     # найдем число обусловленности через встроенную функцию
     A_cond = np.linalg.cond(A, np.inf)
@@ -52,7 +52,8 @@ with open(output_file_path, "a") as f:
         b_error = b_error_list[i]
         x_error = np.linalg.solve(A, b_error)
         d_for_b[i] = np.linalg.norm(x - x_error, ord=np.inf) / np.linalg.norm(x, ord=np.inf)
-    print(f"{d_for_b = }", file=f)
+    # print(f"{d_for_b = }", file=f)
+    pd.DataFrame(d_for_b).to_csv('d_for_b.csv', float_format='%.6f')
 
     # найдем вектор d для изменения A
     d_for_A = np.empty(A.shape)
@@ -61,12 +62,14 @@ with open(output_file_path, "a") as f:
             A_error = A_error_list[i][j]
             x_error = np.linalg.solve(A_error, b)
             d_for_A[i][j] = np.linalg.norm(x - x_error, ord=np.inf) / np.linalg.norm(x, ord=np.inf)
-    print(f"{d_for_A = }", file=f)
+    # print(f"{d_for_A = }", file=f)
+    pd.DataFrame(d_for_A).to_csv('d_for_A.csv', float_format='%.6f')
 
     # найдем что оказывает наибольшее влияние на погрешность для b
     d_for_b_max = d_for_b.max()
     d_for_b_max_index = list(d_for_b).index(d_for_b_max)
-    print(f'{d_for_b_max = } with index = {d_for_b_max_index}', file=f)
+    print(f'Максимальный элемент d для b: {d_for_b_max} с индексом = {d_for_b_max_index}', file=f)
+
 
     # найдем что оказывает наибольшее влияние на погрешность для A
     d_for_A_max = d_for_A.max()
@@ -75,7 +78,7 @@ with open(output_file_path, "a") as f:
         for j in range(n):
             if d_for_A[i][j] == d_for_A_max:
                 d_for_b_max_index = [i, j]
-    print(f'{d_for_A_max = } with index = {d_for_b_max_index}', file=f)
+    print(f'Максимальный элемент d для A: {d_for_A_max} с индексом = {d_for_b_max_index}', file=f)
 
     # построим гистограмму для b
     plt.bar(range(n), d_for_b)
@@ -108,12 +111,14 @@ with open(output_file_path, "a") as f:
         delta_rel_x_error_list[i] = delta_rel_x_error
         # print(f"практически: {d_for_b[i]},\tтеоритически: {delta_rel_x_error}", file=f)
 
-    df_for_b = pd.DataFrame({
-        'Индекс': list(range(n)),
-        'Практические': d_for_b,
-        "Теоретические": delta_rel_x_error_list
-    })
-    df_for_b.to_latex("3.1.8.tex", index=False)
+    print(f'Теоретическая оценка для для b: {A_cond * delta_rel_b}', file=f)
+    # pd.DataFrame(d_for_A).to_csv('d_for_A.csv', float_format='%.6f')
+    # df_for_b = pd.DataFrame({
+    #     'Индекс': list(range(n)),
+    #     'Практические': d_for_b,
+    #     "Теоретические": delta_rel_x_error_list
+    # })
+    # df_for_b.to_latex("3.1.8.tex", index=False)
 
     # сравним погрешности для b
     # print(f"Сравнение погрешностей для A:", file=f)
@@ -126,16 +131,15 @@ with open(output_file_path, "a") as f:
             delta_rel_x_error_list[i][j] = delta_rel_x_error
             # print(f"[{i}][{j}] практически: {d_for_A[i][j]},\tтеоритически: {delta_rel_x_error}", file=f)
 
-    ids = np.empty((n, n), dtype=tuple)
-    for i in range(n):
-        for j in range(n):
-            ids[i][j] = (i, j)
-    df_for_A = pd.DataFrame({
-        'i': ids.reshape(-1),
-        'Практические': d_for_A.reshape(-1),
-        "Теоретические": delta_rel_x_error_list.reshape(-1)
-    })
-    df_for_A.to_latex("3.2.tex", index=False)
+    # ids = np.empty((n, n), dtype=tuple)
+    # for i in range(n):
+    #     for j in range(n):
+    #         ids[i][j] = (i, j)
+    # df_for_A = pd.DataFrame({
+    #     'i': ids.reshape(-1),
+    #     'Практические': d_for_A.reshape(-1),
+    #     "Теоретические": delta_rel_x_error_list.reshape(-1)
+    # })
+    # df_for_A.to_latex("3.2.tex", index=False)
+    print(f'Теоретическая оценка для для A: {A_cond * delta_rel_A}', file=f)
 
-with open(output_file_path, "r") as f:
-    print(f.read())
