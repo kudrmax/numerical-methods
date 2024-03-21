8909007bf9387fd9e54c56211156b1c31dc7aefc
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-21 22:08:29 +0300

    5.5.8 код готов

--- 3/4.1.8/zaid.py
+++ 3/4.1.8/zaid.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 def zaid(A, b, x0, n):
-
     B = np.empty(A.shape, dtype=float)
     for i in range(A.shape[0]):
         for j in range(A.shape[1]):
@@ -24,10 +23,39 @@ def zaid(A, b, x0, n):
     x = x0
     for _ in range(n):
         x_new = np.zeros(x.shape, dtype=float)
-        for i in range(B.shape[0]): # x_new = B1 @ x_new + B2 @ x + c
+        for i in range(B.shape[0]):  # x_new = B1 @ x_new + B2 @ x + c
             x_new[i] = np.sum(B[i][:i] * x_new[:i]) + np.sum(B[i][i:] * x[i:]) + c[i]
         x = x_new
     return x
+
+def relax(A, b, x0, n, w):
+    B = np.empty(A.shape, dtype=float)
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            B[i, j] = - A[i, j] / A[i, i] if i != j else 0
+
+    c = np.empty(b.shape, dtype=float)
+    for i in range(c.shape[0]):
+        c[i] = b[i] / A[i, i]
+
+    # B1 = np.zeros(B.shape)
+    # B2 = np.zeros(B.shape)
+    # for i in range(A.shape[0]):
+    #     for j in range(A.shape[1]):
+    #         if j < i:
+    #             B1[i, j] = B[i, j]
+    #         if j > i:
+    #             B2[i, j] = B[i, j]
+
+    x = x0
+    for _ in range(n):
+        x_new = np.zeros(x.shape, dtype=float)
+        for i in range(B.shape[0]):  # x_new = B1 @ x_new + B2 @ x + c
+            x_new[i] = np.sum(B[i][:i] * x_new[:i]) + np.sum(B[i][i:] * x[i:]) + c[i]
+            x_new[i] = w * x_new[i] + (1 - w) * x[i]
+        x = x_new
+    return x
+
 
 A = np.array([[118.8, -14, -5, -89.1],
               [-59.4, 194, 5, 128.7],
@@ -38,7 +66,8 @@ b = np.array([92.5, -340.1, -898, 184.1])
 
 x_true = np.linalg.solve(A, b)
 x_zaid = zaid(A, b, np.zeros(b.shape[0]), 10)
+x_relax = relax(A, b, np.zeros(b.shape[0]), 10, 1.25)
 
 print(f'{x_true = }')
 print(f'{x_zaid = }')
-
+print(f'{x_relax = }')
